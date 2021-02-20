6127232d259520181ac3bd2dad2a4f99799d0df9
tushar <tushargupta.967@gmail.com>
2021-02-20 21:34:59 +0530

    file added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home/Home'
+
+function App() {
+  return (
+    <div id="colorlib-page">
+        <div id="container-wrap">
+		
+		<div id="colorlib-main">
+      <Home/>
+     
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
